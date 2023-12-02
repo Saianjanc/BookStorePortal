2f030ca043702b4b54b22467bd0edcc658949a0f
Saianjan <saianjan1234@gmail.com>
2023-12-02 19:58:20 +0530

    Added Menu based options for displaying available Books!

--- BookStore.js
+++ BookStore.js
@@ -35,9 +35,25 @@ function displayBooks() {
     console.log(`+----+--------------------+-------+----------+`);
 }
 
-function addBook() {
-    console.log();
+let cart = []
+let total = 0
+
+function addBook(nid) {
+    let name = books[nid-1].name
+    let price = books[nid-1].price
+    books[nid-1].quantity-=1
+    total += price
+    cart.push({name:name,price:price,quantity:1,total:total})
 }
+
+function showCart() {
+    if (total==0) {
+        console.log("Your Cart is Empty");
+    } else {
+        console.log(cart);
+    }
+}
+
 let choice = 0
 while (choice!=4) {
     choice = readline.questionInt("\nEnter Any Number:\n1.Display Books\n2.Add Books to Cart\n3.Show Cart\n4.Exit\n")
@@ -46,7 +62,11 @@ while (choice!=4) {
             displayBooks()
             break;
         case 2:
-            addBook()
+            let nid = readline.questionInt("Enter Book ID to Add to Cart: ")
+            addBook(nid)
+            break;
+        case 3:
+            showCart()
             break;
         case 4:
             console.log("Bye!!\n");
